232c50e6bc5ce21094e1e2f6e5250d6e08d990a2
schubalibre <robertodziuba@googlemail.com>
2015-10-09 08:56:31 +0200

    testCase hinzugefügt

---
+++ src/matVecLib/Vector3Test.java
@@ -0,0 +1,61 @@
+package matVecLib;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by roberto on 09/10/15.
+ */
+public class Vector3Test {
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd1() throws Exception {
+
+    }
+
+    @Test
+    public void testSub() throws Exception {
+
+    }
+
+    @Test
+    public void testMul() throws Exception {
+
+    }
+
+    @Test
+    public void testDot() throws Exception {
+
+    }
+
+    @Test
+    public void testDot1() throws Exception {
+
+    }
+
+    @Test
+    public void testNormalized() throws Exception {
+
+    }
+
+    @Test
+    public void testAsNormal() throws Exception {
+
+    }
+
+    @Test
+    public void testReflectedOn() throws Exception {
+
+    }
+
+    @Test
+    public void testX() throws Exception {
+
+    }
+}
